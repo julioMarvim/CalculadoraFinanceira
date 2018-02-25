803c7b30727434f8283991387f944a2d7d71c8ce
Julio Marvim <juliomarvim@yahoo.com.br>
2018-02-25 20:01:25 -0300

    Add project files.

---
+++ CalculadoraFinanceira/CalculadoraFinanceira.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraFinanceira
+{
+    class CalculadoraFinanceira
+    {
+        public double aplicacao;
+        public double juros;
+        public int meses;
+
+        public void Juros(double Valor)
+        {
+            juros /= 100; 
+        }
+
+        public void ResultadoAplicacao(double valor, int tempo)
+        {
+            for (int i = 1; i <= this.meses; i++)
+            {
+                this.aplicacao += (this.aplicacao * this.juros);   
+            }
+ 
+        }
+        public void AplicacaoEmRFixa(double valor, int tempo)
+        {
+            for (int i = 1; i <= this.meses; i++)
+            {
+                this.aplicacao += (this.aplicacao * this.juros);
+            }
+            //Calculando o imposto de Renda
+            if (this.meses <= 12)
+            {
+                this.aplicacao -= (this.aplicacao * 0.25);
+            }
+            else if (this.meses >= 13 && this.meses <= 24)
+            {
+                this.aplicacao -= (this.aplicacao * 0.15);
+            }
+            else if (this.meses <= 25 && this.meses <= 36)
+            {
+                this.aplicacao -= (this.aplicacao * 0.05);
+            }
+            else
+            {
+                this.aplicacao -= (this.aplicacao * 0.01);
+            }
+        }
+
+    }
+
+}
---
+++ CalculadoraFinanceira/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraFinanceira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tipoDeAplicacao = "Qual tipo de Aplicação deseja Fazer? ";
+            Console.WriteLine(tipoDeAplicacao);
+
+            Console.WriteLine("Para Poupança digite 1: ");
+            Console.WriteLine("Para Renda Fixa digite 2: ");
+            int numeroDaAplicacao = int.Parse(Console.ReadLine());
+
+            if (numeroDaAplicacao == 1)
+            {
+                CalculoPoupanca();
+            } else if (numeroDaAplicacao == 2)
+            {
+                //CalculoRendaFixa();
+            }else
+            {
+                Console.WriteLine("Operação Inválida!");
+                Console.ReadLine();
+            }
+
+        }
+            public static void CalculoPoupanca()
+        {
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine("            ***CALCULADORA FINANCEIRA - POUPANCA***");
+            Console.WriteLine("--------------------------------------------------------------------");
+
+            //Valor a ser aplicado
+            Console.Write("Informe o valor da Aplicação: ");
+            double valorAplicado = double.Parse(Console.ReadLine());
+
+            //% de rendimento mensal na poupança
+            Console.Write("Informe o juros de Poupança: ");
+            double juros = double.Parse(Console.ReadLine());
+
+            //Quantidade de meses que o dinheiro ficará aplicado
+            Console.Write("Infomre por quanto tempo deseja investir: ");
+            int meses = int.Parse(Console.ReadLine());
+
+            CalculadoraFinanceira poupanca = new CalculadoraFinanceira();//Instanciando a classe Poupanca.
+
+            //Armazenando os valores de entrada nos atributos da classe "Poupanca"
+            poupanca.aplicacao = valorAplicado;
+            poupanca.meses = meses;
+            poupanca.juros = juros;
+            poupanca.Juros(poupanca.juros);// armazenando o valor fornecido pelo usuario em um metodo da classe "Poupanca"
+
+            //Chamando o metodo Criado para armazenar a logia da aplicação em poupança
+            poupanca.ResultadoAplicacao(poupanca.aplicacao, poupanca.meses);
+
+            Console.Write("Valor para Aplicação em poupança por {0} meses sera: R${1}", poupanca.meses, Math.Round(poupanca.aplicacao, 2));
+            Console.ReadLine();
+
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine("            ***CALCULADORA FINANCEIRA - RENDA FIXA***");
+            Console.WriteLine("--------------------------------------------------------------------");
+
+            Console.Write("Informe o juros de Renda Fixa: ");
+            double jurosRendaFixa = Convert.ToDouble(Console.ReadLine());
+
+            CalculadoraFinanceira rendaFixa = new CalculadoraFinanceira();
+
+            rendaFixa.aplicacao = valorAplicado;
+            rendaFixa.meses = meses;
+            rendaFixa.juros = juros;
+            rendaFixa.Juros(rendaFixa.juros);
+
+            rendaFixa.AplicacaoEmRFixa(rendaFixa.aplicacao, rendaFixa.meses);
+
+            Console.Write("Valor para Aplicação em poupança por {0} meses sera: R${1}", rendaFixa.meses, Math.Round(rendaFixa.aplicacao, 2));
+            Console.ReadLine();
+            
+
+            if (poupanca.aplicacao > rendaFixa.aplicacao)
+            {
+                Console.Write("Poupança é mais indicada pois terá um maior rendimento.");
+                Console.ReadLine();
+            }else
+            {
+                Console.Write("Renda Fixa é mais indicada pois terá um maior rendimento.");
+                Console.ReadLine();
+            }
+            }
+    }
+}
+    
+
